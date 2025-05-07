f0d05d7573be2ad0d675381dd1b7bbf0af66d016
Ben Osborne <ben.osborne@milliman.com>
2025-05-06 21:44:22 -0500

    Refactor SqsHandler to implement message deserialization and error handling; update service dependencies

--- src/lambdas/PwrTlzDemo/PwrTlzDemo.6.Bat/Services/SqsHandler.cs
+++ src/lambdas/PwrTlzDemo/PwrTlzDemo.6.Bat/Services/SqsHandler.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Amazon.Lambda.SQSEvents;
 using AWS.Lambda.Powertools.BatchProcessing;
 using AWS.Lambda.Powertools.BatchProcessing.Sqs;
@@ -6,12 +7,24 @@ namespace PwrTlzDemo.Services;
 
 internal class SqsHandler : ISqsRecordHandler
 {
-    private readonly RegistrationService _registrationService = new();
+    private readonly HandlerService _handlerService = new();
 
-    private readonly LibraryService _libraryService = new();
-
-    public Task<RecordHandlerResult> HandleAsync(SQSEvent.SQSMessage record, CancellationToken cancellationToken)
+    [Tracing]
+    [Logging(Service = "SqsHandler")]
+    public async Task<RecordHandlerResult> HandleAsync(SQSEvent.SQSMessage record, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var request = JsonSerializer.Deserialize<RegistrationRequest>(record.Body) ??
+            throw new AggregateException("Could not deserialize message to RegistrationRequest");
+
+        try
+        {
+            await _handlerService.ExecuteAsync(request);
+            return RecordHandlerResult.None;
+        }
+        catch (Exception e)
+        {
+            Logger.LogError(e, "Error while processing SQSEvent");
+            throw;
+        }
     }
 }
